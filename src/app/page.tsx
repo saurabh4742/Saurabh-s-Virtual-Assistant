63062a499da0654d6d11ebdf176b2fc0e9bc7906
saurabh anand <saurabhbebi@gmail.com>
2024-02-15 23:40:12 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import SendPromptBar from "@/components/sendPromptBar";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -32,32 +32,36 @@ export default function Home() {
         setLoading(false);
       }
     };
-  
+
     fetchChats();
   }, [messages]);
-  
 
   return (
     <div className="flex justify-center w-full bg-[#0F172A]  text-[#020617] ">
       <div className="flex-none justify-center my-4 items-center min-h-screen pt-4 w-10/12 sm:w-8/12 bg-[#F8FAFC] border-1 shadow-lg border-[#020617]  rounded-3xl">
-        <p className="flex justify-center text-center border-b-2 border-[#020617] rounded-b-md shadow-lg py-4">Saurabh Buddy HERE to Assist YOU!</p>
+        <p className="flex justify-center text-center border-b-2 border-[#020617] rounded-b-md shadow-lg py-4">
+          Saurabh Buddy HERE to Assist YOU!
+        </p>
         <div className="flex-col justify-center w-full">
           <div className="flex-col w-full  justify-center pb-8">
-            {loading ? ( 
-              <Loader/>
-            ) : messages && messages.lastconversations ? ( 
+            {loading ? (
+              <Loader />
+            ) : messages && messages.lastconversations ? (
               messages.lastconversations.map((message, index) => (
                 <div className="px-4" key={index}>
-                  <div className="flex justify-start my-4 text-wrap">User: {message.prompt}</div>
-                  <div className="flex justify-start my-4">AI: {message.responseText}</div>
+                  <div className="flex justify-start my-4 text-wrap">
+                    User: {message.prompt}
+                  </div>
+                  <div className="flex justify-start my-4">
+                    AI: {message.responseText}
+                  </div>
                 </div>
               ))
             ) : (
-              <p>No messages found.</p> 
+              <p>No messages found.</p>
             )}
-             <SendPromptBar/>
+            <SendPromptBar />
           </div>
-         
         </div>
       </div>
     </div>
