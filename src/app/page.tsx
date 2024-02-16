1adcc61faf664adb18c85dcfa5234cc21ec16bda
saurabh anand <saurabhbebi@gmail.com>
2024-02-16 23:26:56 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,8 +38,8 @@ export default function Home() {
   }, [messages]);
 
   return (
-    <div className="flex justify-center w-full bg-[#0F172A]  text-[#020617] ">
-      <div className="flex-none justify-center my-4 items-center min-h-screen pt-4 w-10/12 sm:w-8/12 bg-[#F8FAFC] border-1 shadow-lg border-[#020617]  rounded-3xl">
+    <div className="flex justify-center w-full bg-[#0F172A]  text-[#F8FAFC] ">
+      <div className="flex-none justify-center my-4 items-center min-h-screen pt-4 w-10/12 sm:w-8/12 bg-[#1E293B] border-1 shadow-lg border-[#020617]  rounded-3xl">
         <p className="flex justify-center text-center border-b-2 border-[#020617] rounded-b-md shadow-lg py-4">
           Saurabh Buddy HERE to Assist YOU!
         </p>
@@ -49,12 +49,12 @@ export default function Home() {
               <Loader />
             ) : messages && messages.lastconversations ? (
               messages.lastconversations.map((message, index) => (
-                <div className="px-4" key={index}>
-                  <div className="flex justify-start my-4 text-wrap">
-                    User: {message.prompt}
+                <div className="px-4 text text-wrap" key={index}>
+                  <div className="flex justify-start my-4   whitespace-pre-line">
+                    <strong className="text-cyan-500 mr-2">You:</strong> {message.prompt}
                   </div>
-                  <div className="flex justify-start my-4">
-                    AI: {message.responseText}
+                  <div className="flex justify-start my-4 whitespace-pre-line">
+                  <strong className="text-cyan-500 mr-5">AI:</strong> {message.responseText}
                   </div>
                 </div>
               ))
--- src/components/sendPromptBar.tsx
+++ src/components/sendPromptBar.tsx
@@ -17,27 +17,32 @@ const SendPromptBar = () => {
             const response=await axios.post("/api/gemini",{
                 prompt: prompt
             })
+            const responseText =response.data.responseText
             setLoading(false)
             setPrompt("");
-            toast.success("Respond Successful")
+            if (responseText) {
+              toast.success("Respond Successful")
+            } else {
+              toast.error("Server issue refresh")
+            }
         } catch (error) {
             setLoading(false)
             setPrompt("");
-            toast.error("server issue")
+            toast.error("Server issue")
         }
     }
   return (
     <div className="fixed w-full sm:w-8/12 bottom-6 px-2 ">
-      <div className=" h-[40px] rounded-full  flex sm:w-full justify-start items-center w-10/12 bg-[#0F172A] p-1">
+      <div className=" h-[40px] rounded-full  flex sm:w-full justify-start items-center bg-[#F8FAFC] w-10/12  p-1">
         <input
-          className="text-center text-lg text-wrap  px-2 rounded-full w-full h-[34px]"
+          className="text-center bg-[#0F172A] text-lg text-wrap  px-2 rounded-full  w-full h-[34px]"
           type="text"
           placeholder="Ask anything!"
           value={prompt}
           onChange={handleInputChange}
         />
         {loading? <LoaderForPrompt/> :<button onClick={handlePrompt}>
-          <SendHorizontal className="cursor-pointer" color="#F8FAFC" />
+          <SendHorizontal className="cursor-pointer" color="#0F172A" />
         </button>}
       </div>
     </div>
