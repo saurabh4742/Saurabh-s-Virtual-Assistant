ef4e1a8d3dfe5235ca959b3905708e4acd53635a
saurabh anand <saurabhbebi@gmail.com>
2024-02-17 19:04:17 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,42 +45,60 @@ export default function Home() {
   return (
     <div className="flex justify-center w-full bg-[#0F172A]  text-[#F8FAFC] ">
       <div className="flex-none justify-center my-4 items-center min-h-screen pt-4 w-10/12 sm:w-8/12 border-1 shadow-lg   rounded-3xl">
-      <div className="flex justify-center">
+      {/* <div className="flex justify-center">
       <Suspense fallback={<NewLoader/>}>
+      <div className="flex gap-4 justify-center items-center">
       <img
-      className="rounded-[50%] p-0.5 bg-[#F8FAFC]  border-2 border-[#64748B] shadow-xl"
+      className="rounded-[50%] p-0.5 inset-full  border-2 border-[#64748B] shadow-xl"
         src="https://firebasestorage.googleapis.com/v0/b/image-test-b1876.appspot.com/o/SaurabhAnand111.jpeg?alt=media&token=4a860572-f344-48d4-9b74-511c75489783"
         alt="Avatar"
         height={80}
         width={80}
       />
-      </Suspense>
+      <p>&copy;2024 Saurabh Anand All rights reserved.</p>
       </div>
+      </Suspense>
+      </div> */}
       <Notes/>
       <div>
       <div className="flex-none justify-center my-4 items-center min-h-screen pt-4 w-full bg-[#1E293B] border-1 shadow-lg border-[#020617]  rounded-3xl">
-        <p className="flex justify-center  text-xl text-center border-b-2 border-[#020617] rounded-b-md shadow-lg py-4">
-          Saurabh Buddy HERE to Assist YOU!
+       <div className="flex-none border-b-2 border-[#020617] rounded-b-md shadow-lg  p-4">
+       <p className="flex justify-center  text-3xl text-center  py-4 ">
+       Saurabh Virtual Assistant!
         </p>
+        <div className="flex justify-center">
+      <Suspense fallback={<NewLoader/>}>
+      <div className="flex gap-4 justify-center items-center">
+      <img
+      className="rounded-[50%] p-0.5 inset-full  border-2 border-[#64748B] shadow-xl"
+        src="https://firebasestorage.googleapis.com/v0/b/image-test-b1876.appspot.com/o/SaurabhAnand111.jpeg?alt=media&token=4a860572-f344-48d4-9b74-511c75489783"
+        alt="Avatar"
+        height={80}
+        width={80}
+      />
+      <p>&copy;2024 Saurabh Anand All rights reserved.</p>
+      </div>
+      </Suspense>
+      </div>
+       </div>
         <div className="flex-col justify-center w-full">
-          <div className="flex-col w-full justify-center overflow-x-hidden     pb-8">
+          <div className="flex-col w-full justify-center  pb-8">
             {loading ? (
               <Loader />
             ) : messages && messages.lastconversations ? (
               messages.lastconversations.map((message, index) => (
                 <div className="px-4 text text-wrap" key={index}>
-                  <div className="flex justify-start my-4   whitespace-pre-line">
-                    <strong className="text-cyan-500 mr-2">You:</strong>{" "}
-                    {message.prompt}
+                  <div className="flex justify-start my-4  whitespace-pre-line">
+                    <strong className="text-cyan-500 mr-2">You:</strong>
+                    <div className=" overflow-auto">{message.prompt}</div>
                   </div>
                   <motion.div
                     initial={{ opacity: 0.5 }}
                     animate={{ opacity: [1, 0.5, 1, , 0.5, 1] }}
                     transition={{ ease: "easeIn", duration: 0.2 }}
-                    className="flex justify-start my-4 whitespace-pre-line"
+                    className="flex justify-start my-4 whitespace-pre-line "
                   >
-                    <strong className="text-cyan-500 mr-5">AI:</strong>{" "}
-                    {message.responseText}
+                    <strong className="text-cyan-500 mr-5">AI:</strong><div className=" overflow-auto">{message.responseText}</div>
                   </motion.div>
                 </div>
               ))
--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -1,35 +1,36 @@
+import { AlertCircle, AlertTriangle, NotebookPen, ShieldAlert } from "lucide-react";
 import React from "react";
 
 const Notes = () => {
   return (
-    <div className="flex-none text-sm">
+    <div className="sm:flex flex-none gap-2 sm:overflow-x-hidden text-sm">
       <div
-        className="bg-[#1E293B] border-l-4  rounded-3xl my-2 border-red-500 text-red-700 p-4"
+        className="bg-[#1E293B] border-l-4   rounded-3xl my-2 border-red-500 text-red-700 p-4"
         role="alert"
       >
-        <p className="font-bold">Warning:</p>
-        <p>
+        <p className="font-bold"><AlertTriangle color="#DC2626" /></p>
+        <p className="text-[#F8FAFC]">
           <strong className="text-[11px]">Create New</strong>, will
           erase you previous data.
         </p>
       </div>
       <div
-        className="bg-[#1E293B] border-l-4 rounded-3xl my-2 border-orange-500 text-orange-700 p-4"
+        className="bg-[#1E293B] border-l-4  rounded-3xl my-2 border-orange-500 text-orange-700 p-4"
         role="alert"
       >
-        <p className="font-bold">Important Note:</p>
-        <p>
+        <p className="font-bold"><ShieldAlert color="#F97316" /></p>
+        <p className="text-[#F8FAFC] ">
           Please do not share your personal details or any other sensitive
           information on this website.
         </p>
       </div>
 
       <div
-        className="bg-[#1E293B] w-full border-l-4 my-2 rounded-3xl border-yellow-500 text-yellow-700 p-4"
+        className="bg-[#1E293B]   border-l-4 my-2 rounded-3xl border-yellow-500 text-yellow-700 p-4"
         role="alert"
       >
-        <p className="font-bold">Note:</p>
-        <p>
+        <p className="font-bold"><NotebookPen color="#EAB308" /></p>
+        <p className="text-[#F8FAFC]">
           If you experience server issues after 20 or more conversations, please
           submit a new assistance request.
         </p>
