14e232b5124f1677f4c0b9e47194f7ef884d1c3e
saurabh anand <saurabhbebi@gmail.com>
2024-02-18 23:20:44 +0530

    update

--- src/socketComponent/RealtimeView.tsx
+++ src/socketComponent/RealtimeView.tsx
@@ -1,27 +1,33 @@
-"use client"
 import React, { useEffect, useState } from 'react'
 import toast from 'react-hot-toast'
 import io from 'socket.io-client'
+
 const API_URL = 'https://svi-io.vercel.app'
 const socket = io(API_URL, { autoConnect: false })
 
 const RealtimeView = () => {
   const [views, setViews] = useState(0)
+
   useEffect(() => {
     socket.connect()
+
     socket.on("count", (count) => {
       setViews(count)
       toast.success("Someone Joined")
     })
+
     return () => {
-      toast.remove("Someone Left")
+      socket.off("count")
       socket.disconnect()
+      toast.remove("Someone Left")
     }
-  },[])
+  }, [])
+
   return (
     <div className='flex justify-center'>
-    {views} Instance{views>1 && `${"s"}`} Of This AI Running Currently
-    </div >
+      {views} Instance{views > 1 && `${"s"}`} Of This AI Running Currently
+    </div>
   )
 }
+
 export default RealtimeView
