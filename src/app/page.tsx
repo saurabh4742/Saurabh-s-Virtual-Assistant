27c0aa547d04dae0d7bd3430c532fc16a22f4482
saurabh anand <saurabhbebi@gmail.com>
2024-02-18 21:38:56 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import Notes from "@/components/Notes";
 import TitleAndCopyright from "@/components/TitleAndCopyright";
 import UserInteractive from "@/components/UserInteractive";
-// import RealtimeView from "@/socketComponent/RealtimeView";
+import RealtimeView from "@/socketComponent/RealtimeView";
 export default function Home() {
   return (
     <div className="flex justify-center w-full bg-[#0F172A]  text-[#F8FAFC] ">
       <div className="flex-none justify-center my-4 items-center min-h-screen pt-4 w-10/12 sm:w-8/12 border-1 shadow-lg   rounded-3xl">
         <Notes />
-        {/* <RealtimeView/> */}
+        <RealtimeView/>
         <div>
           <div className="flex-none justify-center my-4 items-center min-h-fit   pt-4 w-full bg-[#1E293B] border-1 shadow-lg border-[#020617]  rounded-3xl">
             <TitleAndCopyright />
