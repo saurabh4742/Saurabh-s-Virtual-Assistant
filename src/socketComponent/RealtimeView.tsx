2168c044884909bf1cd1c72bf5707f7c9419db28
Saurabh Anand <128067046+saurabh4742@users.noreply.github.com>
2024-02-21 00:23:10 +0530

    Update RealtimeView.tsx

--- src/socketComponent/RealtimeView.tsx
+++ src/socketComponent/RealtimeView.tsx
@@ -32,7 +32,7 @@ const RealtimeView = () => {
       socket.off("count")
       socket.off("joined")
       socket.on("disconnect", (reason) => {
-        if(isBetaTester?){
+        if(isBetaTester){
           axios.delete("/api/ekgandpereptapadanasadakpehagtafirega",{
             data: {
               scid:SocketId 
